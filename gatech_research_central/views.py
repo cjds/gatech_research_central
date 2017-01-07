7bb604245b433486b4185bc1c56649e3f220bfde
Carl Saldanha <cjds@live.com>
2017-01-07 00:21:09 -0500

    added study to admin

--- gatech_research_central/views.py
+++ gatech_research_central/views.py
@@ -1,12 +1,12 @@
 from django.http import HttpResponse
 from django.template import loader
-
+from models import Study
 
 def index(request):
-    #latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    study_list = Study.objects.order_by('-published_date')[:5]
     template = loader.get_template('index.html')
     context = {
-        #'latest_question_list': latest_question_list,
+        'study_list': study_list,
     }
     return HttpResponse(template.render(context, request))
 
