954b42d17aadd84b8722097c3ec883f06edc1cf1
Carl Saldanha <cjds@live.com>
2017-01-07 18:19:35 -0500

    connected form to backend

--- gatech_research_central/models.py
+++ gatech_research_central/models.py
@@ -4,11 +4,11 @@ from datetime import datetime, timedelta
 
 class Study(models.Model):
     #author = models.ForeignKey('auth.User')
-    title = models.CharField(max_length=200)
-    text = models.TextField()
-    last_date=models.DateTimeField(default=timezone.now)
+    title = models.CharField(max_length=200,help_text='eg. Robotics study')
+    text = models.TextField(help_text='eg. Our study goes on till the 30th. Visit http://doodle.com for more information')
+    last_date=models. DateField (default=datetime.now()+timedelta(days=30))
     created_date = models.DateTimeField(
-            default=datetime.now()+timedelta(days=30))
+            default=timezone.now)
     published_date = models.DateTimeField(
             blank=True, null=True)
 
