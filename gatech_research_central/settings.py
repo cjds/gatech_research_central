48ad5f2db568b314f765141a2fac2bb29c36a4b5
Carl Saldanha <cjds@live.com>
2017-01-08 00:00:56 -0500

    heroku

--- gatech_research_central/settings.py
+++ gatech_research_central/settings.py
@@ -131,5 +131,4 @@ STATIC_URL = '/static/'
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
-)ango.contrib.staticfiles.finders.AppDirectoriesFinder',
 )
