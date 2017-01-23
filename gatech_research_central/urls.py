7a3d0c908980e05be81ed926bced7310cafabd47
Carl Saldanha <cjds@live.com>
2017-01-22 21:23:11 -0500

    added a astatic

--- gatech_research_central/urls.py
+++ gatech_research_central/urls.py
@@ -23,10 +23,11 @@ from django.conf.urls.static import static
 
 
 
-urlpatterns = [
+urlpatterns = static(settings.STATIC_URL, document_root=settings.STATIC_ROOT,show_indexes= True)
++[
     url(r'^admin/', admin.site.urls),
     url(r'^submit/$', views.submit, name='submit'),
     url(r'^study/(?P<study>\d+)/$', views.study, name='study'),
     url(r'^', views.index, name='index'),
 
-]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT,show_indexes= True)
+]
